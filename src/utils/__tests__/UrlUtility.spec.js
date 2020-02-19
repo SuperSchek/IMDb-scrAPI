5bb65ffc44bd948bed1bf2a6e12b60109dd88514
Sander Schekman <sanderschekman@gmail.com>
2020-02-19 22:28:29 +0100

    test(url utility): asserts scenarios for `buildImdbQuery` method

--- src/utils/__tests__/UrlUtility.spec.js
+++ src/utils/__tests__/UrlUtility.spec.js
@@ -1,7 +1,38 @@
 import faker from "faker";
+const constants = require("../../constants");
 const UrlUtility = require("../UrlUtility");
 
+jest.mock("../../constants");
+
 describe("UrlUtility", () => {
+  describe("buildImdbQuery", () => {
+    it("the passed query is encoded to a URIComponent before being appended", () => {
+      // Assign
+      const query = faker.lorem.words();
+
+      // Act
+      const url = UrlUtility.buildImdbQuery(query);
+
+      // Assert
+      expect(url).toContain(encodeURIComponent(query));
+    });
+    it.each`
+      query                  | type
+      ${faker.lorem.words()} | ${null}
+      ${faker.lorem.words()} | ${"movies"}
+      ${faker.lorem.words()} | ${"tv"}
+      ${faker.lorem.words()} | ${"episode"}
+    `(
+      `makes sure ${constants.QUERY_PLACEHOLDER} is removed from url when query is $query and type is $type`,
+      ({ query, type }) => {
+        // Act
+        const url = UrlUtility.buildImdbQuery(query, type);
+
+        // Assert
+        expect(url).not.toContain(constants.QUERY_PLACEHOLDER);
+      }
+    );
+  });
   describe("buildEpisodeCode", () => {
     it("prepends '0' to numbers under 10", () => {
       // Assign
@@ -25,7 +56,7 @@ describe("UrlUtility", () => {
       // Assert
       expect(episodeCode).toEqual(`S${season}E${episode}`);
     });
-    test.each`
+    it.each`
       season                                     | episode
       ${faker.random.number({ min: 1, max: 9 })} | ${null}
       ${null}                                    | ${faker.random.number({ min: 1, max: 9 })}
