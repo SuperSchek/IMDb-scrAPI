5e112a5cd3adffef0f54c19cf9c6bac92a677da9
Sander Schekman <sanderschekman@gmail.com>
2020-02-24 21:34:04 +0100

    chore: resetting /bin

--- bin/index.js
+++ bin/index.js
@@ -3,18 +3,18 @@ const yargs = require("yargs");
 const Application = require("../src/classes/Application");
 
 yargs
-  .command(
-    "serve",
-    "Start serving the API",
-    yargs => {
-      return yargs.option("port", {
-        describe: "The port the application will listen to",
-        alias: "p",
-        type: "number"
-      });
-    },
-    yargs => {
-      new Application().serve(yargs);
-    }
-  )
-  .help().argv;
+    .command(
+        "serve",
+        "Start serving the API",
+        yargs => {
+            return yargs.option("port", {
+                describe: "The port the application will listen to",
+                alias: "p",
+                type: "number"
+            });
+        },
+        yargs => {
+            new Application().serve(yargs);
+        }
+    )
+    .help().argv;
